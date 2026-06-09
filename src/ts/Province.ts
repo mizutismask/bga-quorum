e7f129e5037d09c334977145df5d8e96b6472d0a
mizutismask <severinek@gmail.com>
2026-06-09 09:21:54 +0200

    feat: add province slots

--- src/ts/Province.ts
+++ src/ts/Province.ts
@@ -7,6 +7,7 @@ import { QuorumCard, QuorumGame, QuorumPlayer } from './types'
  */
 export class Province {
 	private handStock: LineStock<QuorumCard> | null = null
+	private element: HTMLElement
 
 	constructor(
 		private game: QuorumGame,
@@ -16,5 +17,21 @@ export class Province {
             <div id="province-${province}" class="province" data-province="${province}"></div>
         `
 		$('board').insertAdjacentHTML('beforeend', html)
+		this.element = $('province-' + province)
+
+		this.element.insertAdjacentHTML(
+			'beforeend',
+			`
+            <div class="province-slots">
+                ${Array.from({ length: 15 }, (_, i) => `<div class="province-slot slot-${i + 1} ${this.getSlotClasses(i+1)}"></div>`).join('')}
+            </div>
+        `
+		)
+	}
+
+	private getSlotClasses(i: number) {
+		if (i < 7) return 'left-column'
+		if (i > 9) return 'right-column'
+		return ''
 	}
 }
